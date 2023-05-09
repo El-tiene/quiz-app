f23ac6606aa44f1097c1300f2abbc39651609629
El-tiene <85236783+El-tiene@users.noreply.github.com>
2023-05-09 16:19:04 +0200

    API flask Hello World

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
 	x = 'world'
-	return f"Hello, {x}. Welcome !"
+	return f"Hello le sang, {x}. Welcome !"
 
+@app.route('/2')
+def hello_worldbis():
+	x = 'run'
+	return f"Hello le sang, {x}. Welcome !"
 if __name__ == "__main__":
     app.run()
